01a608b5866ea5ac41b70cebfd56d32d2a2994c8
soraride <sorarideblog@gmail.com>
2022-10-25 07:20:54 +0900

    Update seo.tsx

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -45,12 +45,7 @@ const SEO: FC<Props> = ({ title, description }) => {
                         'minimum-scale=1, initial-scale=1, width=device-width',
                 },
             ]}
-        >
-            <link
-                rel='stylesheet'
-                href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'
-            ></link>
-        </Helmet>
+        ></Helmet>
     )
 }
 
